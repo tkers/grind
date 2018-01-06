4e4c968016ff660ccbc3d707981683d28155bc72
Tijn Kersjes <tijn@reaktor.fi>
2018-01-06 02:20:59 +0200

    Using React for rendering game state

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
 
 export default {
@@ -10,8 +13,17 @@ export default {
   },
   plugins: [
     babel({
-      presets: [['env', { modules: false }]],
+      presets: [
+        ['env', { modules: false }],
+        'react'
+      ],
+      exclude: 'node_modules/**',
       babelrc: false
+    }),
+    resolve(),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     uglify()
   ]
